803c0f215250421050b06cf270831560b2bbe32e
KaleStahl <kalestahl@gmail.com>
2024-04-23 13:36:42 -0500

    did some stuff

--- Numeric Simulations/Factorization_Methods.m
+++ Numeric Simulations/Factorization_Methods.m
@@ -15,7 +15,8 @@ tff   = (0:hff:2*pi-hff)-pi;
 %% Shape and Noise Level
 
 name =  'circle';
-noise_percent = 0;
+noise_percent = 1;
+
 %% Run Factorization Method Once
 
 FactorizationMethod(name, noise_percent, N, ht, t, kappa, ffN, hff, tff, false);
@@ -23,11 +24,10 @@ FactorizationMethod(name, noise_percent, N, ht, t, kappa, ffN, hff, tff, false);
 %% Loop over all noise levels and shapes
 
 shape_list = ["circle", "kite", "ellipse"];
-noise_list = 0:.1:1;
+noise_list = 0:.25:2;
 
 for shape = shape_list
     for noise = noise_list
         FactorizationMethod(shape, noise, N, ht, t, kappa, ffN, hff, tff, true);
-        disp(append(name,"_", string(noise), "%noise"))
     end
 end
